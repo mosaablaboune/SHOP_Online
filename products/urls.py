cfbeed5876810db253502ff5d1200575f8529ae1
Mosaab Laboune <51124222+mosaablaboune@users.noreply.github.com>
2021-07-19 02:36:58 +0100

    fix urls in products app

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import product_edit, products_list, product_details, product_add
+from .views import product_edit, product_list, product_details, product_add, product_delete
 
 urlpatterns = [
-    path('products/', products_list, name='products_list'),
+    path('products/', product_list, name='product_list'),
     path('products/add', product_add, name='product_add'),
     path('products/<pk>', product_details, name='product_details'),
-    path('products/edit/pk', product_edit, name='product_edit')
+    path('products/edit/pk', product_edit, name='product_edit'),
+    path('products/delete/pk', product_delete, name='product_delete'),
 ]
